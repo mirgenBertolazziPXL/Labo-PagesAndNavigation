7e0228bbb6ba7ac354ccdb3f0a5e09a07c81a76f
Wim Bervoets <wim.bervoets@pxl.be>
2024-09-15 10:19:57 +0200

    Add project files.

---
+++ HelloPXL/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloPXL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HelloPXL/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HelloPXL
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
